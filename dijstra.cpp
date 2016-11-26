aa96e6795a9d7c51f8ce1e6f7609e7ca061d3c6d
jorgegaray <jorgegaraych@gmail.com>
2016-11-26 18:32:56 -0300

    caminominimo

---
+++ dijstra.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> pi;
+/**********************
+DIJSTRA CAMINO MINIMO
+CON RECONSTRUCION DEL
+CAMINO
+*********************/
+
+int V,E;
+vector < vector <pi> > listAdj;
+vector <pi> dist;
+
+void djistra(int start){
+	int i,l,p,v,u;
+	queue <int> q;
+	q.push(start);
+	dist[start]=make_pair(0,0);
+	while(!q.empty()){
+		v=q.front();
+		l=listAdj[v].size();
+		q.pop();
+		for(i=0; i<l; i++){
+			u=listAdj[v][i].first;  // nodo de llegada
+			p=listAdj[v][i].second;  // peso entre v y u
+
+			if(dist[v].first + p < dist[u].first){
+				dist[u].first=dist[v].first + p;
+				dist[u].second=v;
+				q.push(u);
+			}
+		}
+	}
+}
+
+int main(){
+	int i;
+	int u,v,p;
+
+	scanf("%d %d",&V,&E);
+	listAdj.resize(V+1);
+	dist.resize(V+1);
+	for(i=0;i<=V;i++)
+		dist[i]=make_pair(1e9,0);
+
+	for(i=0; i<E; i++){
+		scanf("%d %d %d",&u,&v,&p);
+		listAdj[u].push_back(make_pair(v,p));
+		listAdj[v].push_back(make_pair(u,p));
+	}
+	djistra(1);
+	printf("Caminos minimos desde el nodo 1\n");
+	for(i=1;i<=V;i++){
+		printf("de 1 a %d: %d  desde: %d\n",i,dist[i].first,dist[i].second);
+	}
+
+	return 0;
+
+}
